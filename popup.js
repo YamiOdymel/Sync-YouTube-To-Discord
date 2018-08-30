a899c0dce9b049e9b86d6c11ec655cbb0262bb86
Tim Speckhals <github@timmyrs.de>
2018-08-30 18:16:19 +0200

    2.2

--- popup.js
+++ popup.js
@@ -1,18 +1,19 @@
-chrome.runtime.sendMessage({action:"ports"},response=>{
-	if(response.discord)
-		document.getElementById("content-ok").className=""
-	else
-		document.getElementById("content-notab").className=""
-	if(response.youtube)
-		document.getElementById("content-source-youtube-ok").className=""
-	else
-		document.getElementById("content-source-youtube-notab").className=""
-	if(response.soundcloud)
-		document.getElementById("content-source-soundcloud-ok").className=""
-	else
-		document.getElementById("content-source-soundcloud-notab").className=""
-	document.getElementById("content-loading").className="hidden"
-})
+//Load & show translations
+let elms=document.querySelectorAll("[data-message]")
+for(let i in elms)
+{
+	let elm=elms[i]
+	if(elm instanceof HTMLElement)
+		elm.textContent=chrome.i18n.getMessage(elm.getAttribute("data-message"))
+}
+elms=document.querySelectorAll("[data-placeholder]")
+for(let i in elms)
+{
+	let elm=elms[i]
+	if(elm instanceof HTMLElement)
+		elm.setAttribute("placeholder",chrome.i18n.getMessage(elm.getAttribute("data-placeholder")))
+}
+//Register change handlers to modify DOM according to selections
 document.getElementById("source").onchange=()=>{
 	let source=document.getElementById("source").value
 	if(source=="custom")
@@ -51,8 +52,32 @@ document.getElementById("type").onchange=()=>{
 		document.getElementById("party").style.display="inline-block"
 	}
 }
-document.getElementById("updatebtn").onclick=()=>
-{
+
+//Query and display current settings
+chrome.storage.local.get(["source","type","name","streamurl","details","state","partycur","partymax"],result=>{
+	if(result.source)
+		document.querySelector("#source [value='"+result.source+"']").setAttribute("selected","selected")
+	document.getElementById("source").onchange();
+	if(result.type)
+		document.querySelector("#type [value='"+result.type+"']").setAttribute("selected","selected")
+	document.getElementById("type").onchange();
+	if(result.name)
+		document.getElementById("name").value=result.name
+	if(result.streamurl)
+		document.getElementById("streamurl").value=result.streamurl
+	if(result.details)
+		document.getElementById("details").value=result.details
+	if(result.state)
+		document.getElementById("state").value=result.state
+	if(result.partycur)
+		document.getElementById("partycur").value=result.partycur
+	if(result.partymax)
+		document.getElementById("partymax").value=result.partymax
+})
+document.getElementById("streamurl").onchange=()=>{//Fix stream url to avoid confusion
+	document.getElementById("streamurl").value=document.getElementById("streamurl").value.replace("www.twitch.tv","twitch.tv")
+}
+document.getElementById("updatebtn").onclick=()=>{//Announce update
 	document.getElementById("updatebtn").setAttribute("disabled","disabled")
 	let source=document.getElementById("source").value
 	chrome.runtime.sendMessage({
@@ -73,24 +98,19 @@ document.getElementById("updatebtn").onclick=()=>
 			document.getElementById("updatebtn").removeAttribute("disabled")
 	})
 }
-document.getElementById("streamurl").onchange=()=>document.getElementById("streamurl").value=document.getElementById("streamurl").value.replace("www.twitch.tv","twitch.tv")
-chrome.storage.local.get(["source","type","name","streamurl","details","state","partycur","partymax"],result=>{
-	if(result.source)
-		document.querySelector("#source [value='"+result.source+"']").setAttribute("selected","selected")
-	document.getElementById("source").onchange();
-	if(result.type)
-		document.querySelector("#type [value='"+result.type+"']").setAttribute("selected","selected")
-	document.getElementById("type").onchange();
-	if(result.name)
-		document.getElementById("name").value=result.name
-	if(result.streamurl)
-		document.getElementById("streamurl").value=result.streamurl
-	if(result.details)
-		document.getElementById("details").value=result.details
-	if(result.state)
-		document.getElementById("state").value=result.state
-	if(result.partycur)
-		document.getElementById("partycur").value=result.partycur
-	if(result.partymax)
-		document.getElementById("partymax").value=result.partymax
+//Query open tab status and show content according to response
+chrome.runtime.sendMessage({action:"ports"},response=>{
+	if(response.discord)
+		document.getElementById("content-ok").className=""
+	else
+		document.getElementById("content-notab").className=""
+	if(response.youtube)
+		document.getElementById("content-source-youtube-ok").className=""
+	else
+		document.getElementById("content-source-youtube-notab").className=""
+	if(response.soundcloud)
+		document.getElementById("content-source-soundcloud-ok").className=""
+	else
+		document.getElementById("content-source-soundcloud-notab").className=""
+	document.getElementById("content-loading").className="hidden"
 })
